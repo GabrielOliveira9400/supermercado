6925c74308ca3d2599dcc20b78630e85436c00f8
Gabriel Oliveira <gabriel.alves9400@hotmail.com>
2021-12-28 22:39:05 -0300

    Cards Products

--- angular/frontend/src/app/components/product/product-card/product-card.component.ts
+++ angular/frontend/src/app/components/product/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-/*import { Product } from "../product.model";*/
+import { Product } from "../product.model";
+import {ProductService} from "../product.service";
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
@@ -7,12 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCardComponent implements OnInit {
 longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
-
-  constructor() { }
+  from Japan..`;
+  products!: Product[]
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productService.read().subscribe(products =>{
+      this.products = products
+      console.log(products)
+    })
   }
 
 }
